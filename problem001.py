2dc1c69294b3a60ec746af40da67c7b47f7e32c2
Johnson <stoiqed@gmail.com>
2025-05-10 16:21:46 +0800

    Solved problem #1

--- problem001.py
+++ problem001.py
@@ -1,1 +1,13 @@
 print("Hello, World, I am back")
+
+total = 0
+for i in range(1,1000):
+    if i % 3 == 0 or i % 5 == 0:
+        total = total + i#print(i)
+        
+print(total)
+
+print("This time I will use inline generator expression")
+print(sum(i for i in range(1,1000) if i % 3 == 0 or i % 5 == 0))
+
+
