8d8dbcd94f30dad3161f3e00f3a43dd93d5f8e51
Johnson <stoiqed@gmail.com>
2025-05-20 19:51:51 +0800

    Practice another method: expand list as required

--- problem016.py
+++ problem016.py
@@ -1,3 +1,5 @@
+print("--- Method 1 ---")
+
 n = 2**1000
 s = str(n)
 total = 0
@@ -8,9 +10,10 @@ print(n)
 print("total:",total)
 
 
+print("--- Method 2 ---")
 # now let's try it with manual arithmetics (no **) as an exercise
-# 2^1000 has 302 digits, so we need to a list of that size, at least
-# we also need a list for carries (just like pencil and paper math)
+# 2^1000 has 302 digits, so we need a list of that size, at least
+# we also need to keep track of the carry
 #
 # refresher:       1                   1
 #                 128                2048
@@ -39,3 +42,34 @@ totalStr = ''.join(str(_) for _ in total[::-1])
 print(totalStr)
 print("total:",sum(int(_) for _ in totalStr))
 
+
+
+
+print("--- Method 3 ---")
+# try another way: double a given list, and expand the list as the number get bigger
+# for example: doubleNums([5, 5])
+#              --> [0, 1, 1]
+# (note: the numbers are reversed in order)
+# but let's also do digit by digit, just like pencil and paper arithmetics
+def doubleNums(nums):
+    answer = []
+    carry = 0
+    # lets double by adding
+    for digit in nums:
+        dd = digit + digit + carry
+        carry = dd // 10
+        remainder = dd % 10
+        answer.append(remainder)
+
+    if carry > 0:
+        answer.append(carry)
+
+    return answer
+
+result3 = [2]
+for _ in range(N-1):
+    result3 = doubleNums(result3)
+
+result3reversed = ''.join(str(_) for _ in result3[::-1])
+print(result3reversed)
+print("total:",sum(int(_) for _ in result3reversed))
