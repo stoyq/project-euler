64d65fb528f89a2e0d4f0bd0ca62228dcad27f99
Johnson <stoiqed@gmail.com>
2025-06-06 18:23:06 +0800

    Solved. Should revisit

---
+++ problem031.py
@@ -0,0 +1,39 @@
+# target amount for problem
+target = 200
+
+ways = {}
+
+# initiazlise ways for 1p
+for amount in range(0, target+1):
+    ways[amount] = 1
+ways[0] = 0
+
+#print(ways)
+
+for coin in [2, 5, 10, 20, 50, 100, 200]:
+    #print(coin)
+    subtotal = {}
+    for ss in range(0, target+1):
+        subtotal[ss] = 0
+
+    for amount in range(coin, target+1):
+
+        # number of times coin can be used to make amount
+        ncoins = amount // coin
+        #print(f"{coin}p can be used {ncoins}x to make {amount}")
+        for j in range(1, ncoins+1):
+            leftover = amount - coin * j
+            #print("\t\tleftover:", leftover)
+            if leftover == 0:
+                subtotal[amount] += 1
+            else:
+                subtotal[amount] += ways[leftover]
+
+    # update ways
+    for key, value in subtotal.items():
+        ways[key] += value
+
+print(f"Number of ways to make {target}p:",ways[target])
+print(ways[77])
+print(ways[137])
+print(ways[199])
