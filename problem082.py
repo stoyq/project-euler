6b064a5c95ea1bb8625aaeddc3a25a5c9b0eb7ff
Johnson <stoiqed@gmail.com>
2025-06-23 22:57:09 +0800

    3 pass bubble propagation

--- problem082.py
+++ problem082.py
@@ -1,4 +1,4 @@
-DEBUG = 0 
+DEBUG = 1
 
 # simple matrix for testing
 M1 = [  [131, 673, 234, 103,  18],
@@ -7,13 +7,19 @@ M1 = [  [131, 673, 234, 103,  18],
         [537, 699, 497, 121, 956],
         [805, 732, 524,  37, 331]
       ]
-
-# Problem 82 input
-with open("./data/0082_matrix.txt", "r", encoding="utf-8") as f:
-    inputText81 = [[ int(d) for d in line.split(',')] for line in f.read().strip().split('\n')]
-
-#print(inputText81)
-M1 = inputText81
+M1 = [
+    [1, 100, 100, 100, 100],
+    [1,   1,   1,   1,   1],
+    [1, 100,   1, 100, 100],
+    [1, 100,   1, 100, 100],
+    [1, 100,   1,   1,   1]
+]
+## Problem 82 input
+#with open("./data/0082_matrix.txt", "r", encoding="utf-8") as f:
+#    inputText81 = [[ int(d) for d in line.split(',')] for line in f.read().strip().split('\n')]
+#
+##print(inputText81)
+#M1 = inputText81
 
 #for i in range(5):
 #    for j in range(5):
@@ -21,6 +27,9 @@ M1 = inputText81
 #    print()
 
 size = len(M1)
+
+# method 1
+print("Method 1")
 
 # store shortest path to i,j here
 # this below is a deep copy
@@ -60,7 +69,8 @@ for col in range(1, size):
             for b in range(row+2, size):
                 below_addup += M1[b][col]
                 below = min(below, below_addup + SP[b][col-1])
-        
+       
+        min_path_value = -1
         if hasAbove and hasBelow:
             min_path_value = min(left, above, below)
 
@@ -72,11 +82,6 @@ for col in range(1, size):
 
         
         SP[row][col] = min_path_value + M1[row][col]
-
-
-
-
-
 
 
 min_path_sum = min(v[-1] for v in SP)
@@ -91,3 +96,46 @@ if DEBUG:
         print()
 
 print(f"The minimum path sum is: {min_path_sum}")
+
+
+print("Method 2")
+# method 2
+# store shortest path to i,j here
+# this below is a deep copy
+SP2 = [row[:] for row in M1]
+
+for col in range(1, size):
+    #print(f"Processing column {col}")
+
+    # pass 1: minimum from left
+    #
+    #print("Pass 1: min from left")
+    for row in range(size):
+        SP2[row][col] = SP2[row][col-1] + M1[row][col]
+
+    # pass 2: minimum from above (start from top and bubble downwards)
+    #
+    for row in range(1, size):
+        min_so_far = SP2[row][col]
+        if (SP2[row-1][col] + M1[row][col]) < min_so_far:
+            SP2[row][col] = SP2[row-1][col] + M1[row][col]
+
+    # pass 3: minimum from below (start from bottom and bubble upwards)
+    #
+    for row in range(size-2, -1, -1):
+        min_so_far = SP2[row][col]
+        if (SP2[row+1][col] + M1[row][col]) < min_so_far:
+            SP2[row][col] = SP2[row+1][col] + M1[row][col]
+
+
+if DEBUG:
+    for i in range(size):
+        for j in range(size):
+            print("%4d " % SP2[i][j], end='')
+            if j == size - 1:
+                min_path_sum = min(min_path_sum, SP2[i][j])
+
+        print()
+
+min_path_sum2 = min(v[-1] for v in SP2)
+print(f"The minimum path sum is: {min_path_sum2}")
